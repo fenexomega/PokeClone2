9e22c11c5fb9a4e2050fb0a089c7e74c46705694
Jordy <jordyfgomes@gmail.com>
2015-03-21 00:31:33 -0300

    ...

--- cross_compile_win.sh
+++ cross_compile_win.sh
@@ -2,4 +2,4 @@
 mkdir build
 cd build
 cmake .. -DCMAKE_TOOLCHAIN_FILE=/home/jordy/Projects/Cpp/mingw/Toolchain-mingw-i686.cmake -DSDL2_INCLUDE_DIR=/home/jordy/Projects/Cpp/mingw/usr/include/SDL2
-
+make game 
