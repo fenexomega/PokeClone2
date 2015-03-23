cbcedf0434e0500a429fd20c9309ee0b38ad2956
Jordy <jordyfgomes@gmail.com>
2015-03-23 14:34:39 -0300

    new player sprite

--- Contents/MainGame/scripts/playerInput.lua
+++ Contents/MainGame/scripts/playerInput.lua
@@ -19,14 +19,11 @@ function Update(obj,dt)
 
     if isKeyPressed(keys["up"]) then
         acc.y = acc.y - acceleration
-    end
-    if isKeyPressed(keys["down"]) then
+	elseif isKeyPressed(keys["down"]) then
         acc.y = acc.y + acceleration
-    end
-    if isKeyPressed(keys["left"]) then
+	elseif isKeyPressed(keys["left"]) then
         acc.x = acc.x + acceleration
-    end
-    if isKeyPressed(keys["right"]) then
+	elseif isKeyPressed(keys["right"]) then
         acc.x = acc.x -acceleration
     end
 
