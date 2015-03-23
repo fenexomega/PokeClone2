b1027e479748d6c51eb4f2aba13812baecbed0f1
Jordy <jordyfgomes@gmail.com>
2015-03-23 19:03:49 -0300

    nova class dialog

---
+++ src/game/main/objects/Dialog.cpp
@@ -0,0 +1,13 @@
+#include "Dialog.h"
+
+Dialog::Dialog(std::string text, ptr_font font)
+    : _text(text),_font(font)
+{
+
+}
+
+Dialog::~Dialog()
+{
+
+}
+
---
+++ src/game/main/objects/Dialog.h
@@ -0,0 +1,24 @@
+#ifndef DIALOG_H
+#define DIALOG_H
+
+#include <iostream>
+#include <memory>
+
+class Font;
+
+
+using std::shared_ptr;
+using ptr_font = shared_ptr<Font>;
+
+
+class Dialog
+{
+private:
+    std::string _text;
+    ptr_font _font;
+public:
+    Dialog(std::string text,ptr_font font);
+    ~Dialog();
+};
+
+#endif // DIALOG_H
